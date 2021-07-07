598f48dbc9fe630b8c46a0ddd8bc179f070ff43f
Kenneth Doan <kennethxdoan@gmail.com>
2021-07-07 16:44:13 -0700

    Added Feed component

--- client/src/components/UserFeedResult.jsx
+++ client/src/components/UserFeedResult.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
 const userFeedResult = (props) => {
-  const { newUserFeed } = props;
+  const { videoEntry, index } = props;
+  const formatTime = new Date(videoEntry.createTime * 1000).toString().slice(0, 16);
 
   return (
-    <div>
-      results table of last 100 videos
-    </div>
+    <tr id={videoEntry.id}>
+      <td>{index + 1}</td>
+      <td>{videoEntry.text}</td>
+      <td>{videoEntry.playCount.toLocaleString('en-US')}</td>
+      <td>{videoEntry.diggCount.toLocaleString('en-US')}</td>
+      <td>{videoEntry.commentCount.toLocaleString('en-US')}</td>
+      <td>{formatTime}</td>
+      <td>{videoEntry.webVideoUrl}</td>
+    </tr>
   );
 };
 export default userFeedResult;
